131db2ff42a43ab7559bc5c859b642ed15a5dbff
JacquelineHockin <jacquelinehcashman@gmail.com>
2022-12-14 22:11:13 -1000

    finishes get routes for categories

--- Develop/server.js
+++ Develop/server.js
@@ -13,7 +13,7 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 sequelize
   .sync({
-    force: false,
+    force: true,
   })
   .then(function () {
     app.listen(PORT, () => {
